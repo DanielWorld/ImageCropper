b496de2260ca15d955b102a4d13b6213de426695
Daniel Park <op7773hons@gmail.com>
2017-07-14 14:16:42 +0900

    fix tag error

--- library/src/main/java/com/danielpark/imagecropper/CropExtension.java
+++ library/src/main/java/com/danielpark/imagecropper/CropExtension.java
@@ -3,7 +3,7 @@ package com.danielpark.imagecropper;
 /**
  * Set crop image's file extension <br>
  *     <b> jpg : </b> more efficient for small bitmap or thumbnail <br>
- *     <b> png : </b> lossless quality. And it represents transparent image </b> <br>
+ *     <b> png : </b> lossless quality. And it represents transparent image <br>
  * <br><br>
  * Copyright (C) 2014-2017 daniel@bapul.net
  * Created by Daniel on 2017-07-13.
